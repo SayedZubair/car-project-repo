6d59e5adea30e064a0f755627d0c3088c557e5a2
SayedZubair <sayedzubairhashimi2020>
2022-12-19 18:15:59 -0800

    the feature of the car have been added successfully

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
         Carfeatures xx=new Carfeatures();
         System.out.println("lounch the car");
+        xx.color="red";
+        xx.door="4";
+        xx.engine=2222;
+        xx.model="a5";
     }
 }
