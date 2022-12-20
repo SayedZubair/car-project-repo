1aa1486d1573a0352f763eae20bb41128b6fb95e
SayedZubair <sayedzubairhashimi2020>
2022-12-19 17:53:22 -0800

    basic requirment is compelete launching the car

---
+++ src/Carfeatures.java
@@ -0,0 +1,7 @@
+public class Carfeatures {
+    int engine;
+    String model;
+    String color;
+    String door;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Carfeatures xx=new Carfeatures();
+        System.out.println("lounch the car");
+    }
+}
